207681ada59f34c9208c028e72a9a8e8d76d9eee
Deepak Kulkarni <deepak.kulkarni.learning@gmail.com>
2024-03-10 12:52:13 +0530

    Singleton Design Pattern Implemented

---
+++ Creational Patterns/Singleton/Greeting.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal sealed class Greeting
+{
+    private Greeting()
+    {
+    }
+
+    public static Greeting Instance = new Greeting();
+}
---
+++ Creational Patterns/Singleton/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+var greeting1 = Greeting.Instance;
+var greeting2 = Greeting.Instance;
+
+Console.WriteLine(object.ReferenceEquals(greeting1, greeting2));
